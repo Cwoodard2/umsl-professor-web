3965f76d6eb8056bb40083493a6fe5632508b90a
Cwoodard2 <cawooda@siue.edu>
2022-12-22 14:38:43 -0600

    Basic homepage info component added

--- src/components/HomepageInfo.tsx
+++ src/components/HomepageInfo.tsx
@@ -3,14 +3,15 @@ import NavButton from "./NavButton";
 
 const HomepageInfo = (props: any) => {
     return (
-        <div className="w-screen v-screen">
-            <div className="flex flex-row">
+        <div className="w-screen h-screen flex flex-row justify-center items-center">
+                <img></img>
                 <div className="flex flex-col">
-                    <h1></h1>
-                    <p></p>
+                    <h1>{props.title}</h1>
+                    <p>{props.description}</p>
                     <NavButton PlaceToGo={props.goingWhere}/>
                 </div>
             </div>
-        </div>
     );
 };
+
+export default HomepageInfo;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import StandardPage from "../components/StandardPage";
+import HomepageInfo from "../components/HomepageInfo";
 
 const Homepage = (props: any) => {
     return (
         <StandardPage>
-            <h1 className="text-green-600">This is the start of something great!</h1>
+            <h1 className="text-green-600 h-80">This is the start of something great!</h1>
+            <HomepageInfo title='Teaching' description='Teaching description' goingWhere='Learn More'/>
+            <HomepageInfo title='Research' description='Research description' goingWhere='Learn More'/>
+            <HomepageInfo title='Community Engagement' description='Community Engagement description' goingWhere='Learn More'/>
         </StandardPage>
     );
 };
