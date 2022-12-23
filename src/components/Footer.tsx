958e2954edb6e8a533911e9dc972342277e156b5
Cwoodard2 <cawooda@siue.edu>
2022-12-23 16:41:33 -0600

    Added the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="bg-slate-500 w-screen h-20">
+
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/StandardPage.tsx
+++ src/components/StandardPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "./Navigation";
+import Footer from "./Footer";
 
 const StandardPage = (props: any) => {
     return (
         <div>
             <Navigation />
             {props.children}
+            <Footer />
         </div>
     );
 };
