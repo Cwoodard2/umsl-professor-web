ac9e50ca6312011008cc6f4584df7895e2a1fa1b
Cwoodard2 <cawooda@siue.edu>
2022-12-28 15:08:49 -0600

    Added UI for search bar in research

--- src/components/ResearchItems.tsx
+++ src/components/ResearchItems.tsx
@@ -9,9 +9,9 @@ const ResearchItems = (props: any) => {
                 <h3 className="rockwell text-xl">{props.articleTitle}</h3>
                 <p>{props.abstract}</p>
                 <div className="flex flex-row gap-10">
-                    <button className="bg-white border-webGreen border-2 rounded-md p-1 text-webGreen hover:text-white hover:bg-webGreen transition-all">PDF</button>
-                    <button className="bg-white border-webGreen border-2 rounded-md p-1 text-webGreen hover:text-white hover:bg-webGreen transition-all">DOI</button>
-                    <button className="bg-white border-webGreen border-2 rounded-md p-1 text-webGreen hover:text-white hover:bg-webGreen transition-all">CITE</button>
+                    <button className="bg-white border-webGreen border-2 rounded-md pl-2 pr-2 text-webGreen hover:text-white hover:bg-webGreen transition-all font-bold pt-0">PDF</button>
+                    <button className="bg-white border-webGreen border-2 rounded-md p-1 text-webGreen hover:text-white hover:bg-webGreen transition-all font-bold">DOI</button>
+                    <button className="bg-white border-webGreen border-2 rounded-md p-1 text-webGreen hover:text-white hover:bg-webGreen transition-all font-bold">CITE</button>
                 </div>
             </div>
         </div>
---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = (props: any) => {
+
+    const handleChange = () => {
+        return;
+    }
+    
+    return (
+        <>
+            <input placeholder="Type Here to search" className="bg-white border-webGreen border-2 rounded-full shadow-lg p-2"></input>
+        </>
+    );
+};
+
+export default SearchBar;
