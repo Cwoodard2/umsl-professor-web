30f189a3d4957653a88b566c17d7348962a5f29a
Cwoodard2 <cawooda@siue.edu>
2022-12-29 11:12:42 -0600

    Continued to flush out the philosophy tile

--- src/components/PhilosophyTile.tsx
+++ src/components/PhilosophyTile.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const PhilosophyTile = (props: any) => {
     return (
-        <></>
+        <div className="border-webBlue border-2 w-1/4 flex flex-col justify-start items-center p-1 rounded-md">
+            <div className="bg-webGreen rounded-full w-12 h-12 rockwell flex justify-center items-center text-white text-3xl">{props.number}</div>
+            <h3 className="rockwell text-xl">{props.philosophy}</h3>
+            <p>{props.philDescript}</p>
+        </div>
     );
 };
 
--- src/pages/Teaching.tsx
+++ src/pages/Teaching.tsx
@@ -23,6 +23,11 @@ const Teaching = () => {
                 </div>
                 <img src={teachingImage} className="w-1/3 h-3/5 rounded-md shadow-md object-cover" alt="Elaina Johns-Wolfe teaching"></img>
             </div>
+            <div className="pl-16 pr-16 flex flex-row justify-around">
+                <PhilosophyTile number="1" philDescript="Description of your philsophy 1" philosophy="Student Led Teaching"/>
+                <PhilosophyTile number="2" philDescript="Description of your philsophy 2" philosophy="Learning By Working"/>
+                <PhilosophyTile number="3" philDescript="Description of your philsophy 3" philosophy="Letting You Be heard"/>
+            </div>
             <div className="p-16">
                 <ClassCard class="2202" whenTaught="Spring"/>
             </div>
