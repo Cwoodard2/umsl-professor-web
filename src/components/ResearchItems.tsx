573f9b84b5863908274f1c9a1321fca52e81ee2c
Cwoodard2 <cawooda@siue.edu>
2023-01-02 13:54:06 -0600

    Still working on improving mobile responsiveness

--- src/components/ResearchItems.tsx
+++ src/components/ResearchItems.tsx
@@ -3,8 +3,8 @@ import researchImage from "../images/research2.jpeg";
 
 const ResearchItems = (props: any) => {
     return (
-        <div className="flex flex-row p-16 gap-10">
-            <img src={researchImage} className="w-1/3 h-3/5 rounded-md shadow-lg object-cover" alt="Placeholder alt text"/>
+        <div className="flex flex-col-reverse sm:flex-row p-16 gap-10">
+            <img src={researchImage} className="w-3/3 h-auto md:w-1/3 md:h-3/5 rounded-md shadow-lg object-cover" alt="Placeholder alt text"/>
             <div className="flex flex-col">
                 <h3 className="rockwell text-xl">{props.articleTitle}</h3>
                 <p>{props.abstract}</p>
