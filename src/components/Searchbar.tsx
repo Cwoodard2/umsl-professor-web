7236d7e0044d6c6c56ac745ae1779b01f28df1e5
Cwoodard2 <cawooda@siue.edu>
2023-01-16 12:13:45 -0600

    Implemented rudimentry search system

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 
 const SearchBar = (props: any) => {
-    let filterArray = [];
+  let filterArray = [];
 
-    const handleChange = () => {
-        return;
-    }
-    
-    return (
-        <>
-            <input placeholder="Type Here to search" className="bg-white border-webGreen border-2 rounded-full shadow-lg p-2"></input>
-        </>
-    );
+  const handleChange = () => {
+    props.filter("article 1");
+    return;
+  };
+
+  return (
+    <>
+      <input
+        placeholder="Type Here to search"
+        className="bg-white border-webGreen border-2 rounded-full shadow-lg p-2"
+      ></input>
+      <button onClick={() => handleChange()}></button>
+    </>
+  );
 };
 
 export default SearchBar;
--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -1,49 +1,81 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import StandardPage from "../components/StandardPage";
-import researchImage from '../images/research2.jpeg';
+import researchImage from "../images/research2.jpeg";
 import ResearchItems from "../components/ResearchItems";
 import SearchBar from "../components/Searchbar";
-import {db} from "../data/firebaseConfiguration";
+import { db } from "../data/firebaseConfiguration";
 
 const Research = () => {
-    const [articles, setResearchArticles] = useState([]);
+  const [articles, setResearchArticles] = useState([]);
+  const [finalArticles, setFinalArticles] = useState<any>([]);
 
-    useEffect(() => {
-        const checkForData = async () => {
-            const docToGet: any = doc(db, 'professordata', 'research');
-            const researchDoc: any = await getDoc(docToGet);
-            const data = researchDoc.data();
-            setResearchArticles(data.researchArticles);
-        }
-        checkForData();
-    }, []);
+  useEffect(() => {
+    const checkForData = async () => {
+      const docToGet: any = doc(db, "professordata", "research");
+      const researchDoc: any = await getDoc(docToGet);
+      const data = researchDoc.data();
+      setResearchArticles(data.researchArticles);
+    };
+    checkForData();
+  }, []);
 
-    const makeList = () => {
-        const articleArray = articles.map((article: any) => <li className="list-none"><ResearchItems articleTitle={article.title} abstract={article.abstract} articleLink={article.articleLink} authors={article.authors}/></li>);
-        console.log(articleArray);
-        return articleArray;
-    }
-    console.log(articles[0]);
+  const makeList = () => {
+    const articleArray = articles.map((article: any) => (
+      <li className="list-none">
+        <ResearchItems
+          articleTitle={article.title}
+          abstract={article.abstract}
+          articleLink={article.articleLink}
+          authors={article.authors}
+        />
+      </li>
+    ));
+    console.log(articleArray);
+    setFinalArticles(articleArray);
+  };
 
-    const articlesToShow = makeList();
-    console.log(articlesToShow);
-
-    return (
-        <StandardPage>
-            <div className="w-screen h-screen flex flex-col md:flex-row justify-around items-start bg-white p-16 gap-10">
-                <div className="flex flex-col">
-                    <h1 className="text-webGreen rockwell text-4xl">Research</h1>
-                    <p className="text-black">Discover the research that I partake in and the potential effects of it.</p>
-                    <SearchBar />
-                </div>
-                <img src={researchImage} className="w-3/3 h-3/5 md:w-1/3 md:h-3/5 rounded-md shadow-md object-cover" alt="Elaina Johns-Wolfe"></img>
-            </div>
-            <div className="flex flex-col">
-            </div>
-            <div>{articlesToShow}</div>
-        </StandardPage>
+  const filterList = (filterCriteria: string) => {
+    const filteredList = articles.filter((article: any) =>
+      article.title.includes(filterCriteria)
     );
+    const finalList = filteredList.map((article: any) => (
+      <li className="list-none">
+        <ResearchItems
+          articleTitle={article.title}
+          abstract={article.abstract}
+          articleLink={article.articleLink}
+          authors={article.authors}
+        />
+      </li>
+    ));
+    setFinalArticles(finalList);
+  };
+
+  const articlesToShow = makeList();
+  console.log(articlesToShow);
+
+  return (
+    <StandardPage>
+      <div className="w-screen h-screen flex flex-col md:flex-row justify-around items-start bg-white p-16 gap-10">
+        <div className="flex flex-col">
+          <h1 className="text-webGreen rockwell text-4xl">Research</h1>
+          <p className="text-black">
+            Discover the research that I partake in and the potential effects of
+            it.
+          </p>
+          <SearchBar filter={filterList} />
+        </div>
+        <img
+          src={researchImage}
+          className="w-3/3 h-3/5 md:w-1/3 md:h-3/5 rounded-md shadow-md object-cover"
+          alt="Elaina Johns-Wolfe"
+        ></img>
+      </div>
+      <div className="flex flex-col"></div>
+      <div>{finalArticles}</div>
+    </StandardPage>
+  );
 };
 
 export default Research;
