012b63ee5bf36f9252f7dd9c4878a2dc3f82a64a
Cwoodard2 <cawooda@siue.edu>
2023-01-21 19:44:10 -0600

    Added preview for class card editor

--- src/CMSBackEnd/ClassCardEditor.jsx
+++ src/CMSBackEnd/ClassCardEditor.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
 import CMSNav from "../components/CMSNav";
+import ClassCard from "../components/ClassCard";
 import {db} from "../data/firebaseConfiguration";
 import { arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
 
 const ClassCardEditor = () => {
     const [articleAuthors, setAuthors] = useState([]);
+    const [card, setCard] = useState("");
+    const [description, setDescription] = useState("");
     // type researchToAdd = {
     //     title: any,
     //     abstract: any,
@@ -20,10 +23,14 @@ const ClassCardEditor = () => {
 
     const handleSubmit = async () => {
         const toSave = {
-            title: document.getElementById("class").value,
-            abstract: document.getElementById("description").value,
+            title: card,
+            abstract: description,
             authors: articleAuthors,
         }
+
+        setCard("");
+        setDescription("");
+        setAuthors([]);
 
         const docRef = doc(db, "professordata", 'classCard');
         await updateDoc(docRef, {
@@ -37,13 +44,18 @@ const ClassCardEditor = () => {
         <CMSNav />
         <div className="flex flex-col gap-10 justify-center items-center">
             <h1>Class Card</h1>
-            <input placeholder="Class" id="class" className="border border-webGreen rounded-sm p-1"></input>
-            <textarea placeholder="Course Description" id="description" className="border border-webGreen rounded-sm p-1"></textarea>
+            <input placeholder="Class" id="class" className="border border-webGreen rounded-sm p-1" value={card} onChange={(e) => setCard(e.target.value)}></input>
+            <textarea placeholder="Course Description" id="description" className="border border-webGreen rounded-sm p-1" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
             <input placeholder="Semesters Taught" id="whenTaught" className="border border-webGreen rounded-sm p-1"></input>
             <ul>{articleAuthors.map((author) => <li>{author}</li>)}</ul>
             <button className="bg-webGreen rounded-sm pl-1 pr-1 text-white" onClick={() => addItem()}>Add Semester</button>
             <button className="bg-webGreen rounded-sm pl-1 pr-1 text-white" onClick={() => handleSubmit()}>Submit</button>
         </div>
+        <ClassCard
+            class={card}
+            whenTaught={articleAuthors}
+            descript={description}
+          />
         </>
     );
 };
