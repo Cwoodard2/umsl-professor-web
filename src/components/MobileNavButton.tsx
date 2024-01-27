9189f3228d7fdde20f4eb2563b6ba53ab5a1ddba
Cwoodard2 <cawooda@siue.edu>
2024-01-26 23:03:58 -0600

    Improved look of mobile navigation

---
+++ src/components/MobileNavButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const MobileNavButton = (props: any) => {
+  let styles: string;
+  if (props.colors === "white") {
+    styles =
+      "border-white border-2 p-2 rounded-md text-white hover:text-webGreen hover:bg-white transition-all font-bold";
+  } else {
+    styles =
+      "border-webGreen border-2 p-2 rounded-md text-webGreen hover:text-white hover:bg-webGreen transition-all font-bold";
+  }
+
+  return (
+    <Link to={props.path}>
+      <button className={styles}><div className="flex flex-row gap-1">{props.icon}{props.PlaceToGo}</div></button>
+    </Link>
+  );
+};
+
+export default MobileNavButton;
